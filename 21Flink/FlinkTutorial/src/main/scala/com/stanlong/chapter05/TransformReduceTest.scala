ae8f4c75fbad540f21f2bce95105d063f0a38131
Administrator <gitlab_admin_6d7a93@example.com>
2024-12-24 21:37:14 +0800

    聚合

---
+++ 21Flink/FlinkTutorial/src/main/scala/com/stanlong/chapter05/TransformReduceTest.scala
@@ -0,0 +1,18 @@
+package com.stanlong.chapter05
+
+import org.apache.flink.streaming.api.scala._
+object TransformReduceTest {
+    def main(args: Array[String]): Unit = {
+        val env = StreamExecutionEnvironment.getExecutionEnvironment
+        env.setParallelism(1)
+
+        env.addSource(new ClickSource)
+                .map(r => (r.user, 1L))
+                .keyBy(_._1)   //按照用户名进行分组
+                .reduce((r1, r2) => (r1._1, r1._2 + r2._2))  //计算每个用户的访问频次
+                .keyBy(_ => true)     //将所有数据都分到同一个分区
+                .reduce((r1, r2) => if (r1._2 > r2._2) r1 else r2)    //通过 reduce 实现 max 功能，计算访问频次最高的用户
+                .print()
+        env.execute()
+    }
+}
