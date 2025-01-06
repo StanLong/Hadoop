31c9a816ad6027fb87d2f400d98ffe0f9837ec5c
Administrator <gitlab_admin_6d7a93@example.com>
2025-01-06 22:17:18 +0800

    统计窗口时间内url的点击次数

---
+++ 21Flink/FlinkTutorial/src/main/scala/com/stanlong/chapter06/UrlViewCount.scala
@@ -0,0 +1,53 @@
+package com.stanlong.chapter06
+
+import com.stanlong.chapter05.{ClickSource, Event}
+import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.java.aggregation.AggregationFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+object UrlViewCount {
+    def main(args: Array[String]): Unit = {
+        val env = StreamExecutionEnvironment.getExecutionEnvironment
+        env.setParallelism(1)
+
+        val stream = env.addSource(new ClickSource)
+          .assignAscendingTimestamps(data => data.timestamp)
+
+        stream.keyBy(data => data.url)
+          .window(SlidingEventTimeWindows.of(Time.seconds(10), Time.seconds(5)))
+          .aggregate(new UrlViewCountAgg, new UrlViewCountResult) // 统计窗口时间内 url 被点击的次数
+          .print()
+
+        env.execute()
+
+    }
+
+    private class UrlViewCountAgg extends AggregateFunction[Event, Long, Long] {
+
+        override def createAccumulator(): Long = 0L
+
+        override def add(in: Event, acc: Long): Long = acc + 1 // 来一个数据加一个数据
+
+        override def getResult(acc: Long): Long = acc
+
+        override def merge(acc: Long, acc1: Long): Long = ???
+    }
+
+    private class UrlViewCountResult extends ProcessWindowFunction[Long, UrlView, String, TimeWindow] {
+        override def process(key: String, context: Context, elements: Iterable[Long], out: Collector[UrlView]): Unit = {
+            val count = elements.iterator.next()
+            val start = context.window.getStart
+            val end = context.window.getEnd
+
+            out.collect(UrlView(key, count, start, end))
+        }
+    }
+
+}
+
+case class UrlView(url : String, count : Long, windowStart: Long, windowEnd : Long)
