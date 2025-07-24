795ccc3f764eda4ac3480dd2f9075cc231ae932f
Administrator <gitlab_admin_6d7a93@example.com>
2025-07-24 20:07:57 +0800

    目录调整

--- 08clickhouse/clickhouse_java/src/main/java/com/stanlong/clickhouse/dao/IotDataMapper.java
+++
@@ -1,7 +0,0 @@
-package com.stanlong.clickhouse.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.stanlong.clickhouse.entity.IotData;
-
-public interface IotDataMapper extends BaseMapper<IotData> {
-}
--- 08clickhouse/clickhouse_java/src/main/java/com/stanlong/clickhouse/service/IotDataService.java
+++
@@ -1,7 +0,0 @@
-package com.stanlong.clickhouse.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.stanlong.clickhouse.entity.IotData;
-
-public interface IotDataService extends IService<IotData> {
-}
